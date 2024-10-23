3c846f05e624e8f6d45378771f9e36db168d124a
Pas <116931403+pasdeveloper@users.noreply.github.com>
2024-10-23 11:26:49 +0200

    first commit

---
+++ free_fireship.js
@@ -0,0 +1,40 @@
+function hackFireship() {
+  const videoPlayer = document.querySelector("video-player");
+
+  if (!videoPlayer) {
+    console.error("<video-player> element not found.");
+    return;
+  }
+
+  const globalData = document.querySelector("global-data");
+
+  let videoSrc;
+
+  if (globalData["vimeo"]) {
+    videoSrc = "https://player.vimeo.com/video/" + globalData["vimeo"];
+  } else if (globalData["youtube"]) {
+    videoSrc = "https://youtube.com/embed/" + globalData["youtube"];
+  } else {
+    console.error("no video data found");
+    return;
+  }
+
+  videoPlayer.setAttribute("free", "true");
+
+  const iframeHTML = `<iframe src="${videoSrc}" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen width="426" height="240" frameborder="0"></iframe>`;
+
+  videoPlayer.innerHTML = iframeHTML;
+
+  const placeholder = document.createElement("div");
+  videoPlayer.parentNode.insertBefore(placeholder, videoPlayer);
+
+  const parent = videoPlayer.parentNode;
+  parent.removeChild(videoPlayer);
+  parent.insertBefore(videoPlayer, placeholder);
+
+  placeholder.remove();
+
+  console.log(iframeHTML);
+}
+
+hackFireship();
